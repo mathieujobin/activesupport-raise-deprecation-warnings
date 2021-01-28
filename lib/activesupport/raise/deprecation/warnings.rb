639d53da139e4e72ffc61a9e1f9acd50566087ef
Mathieu Jobin <majobin@mdsol.com>
2021-01-28 15:25:01 +0900

    replace blank?

--- lib/activesupport/raise/deprecation/warnings.rb
+++ lib/activesupport/raise/deprecation/warnings.rb
@@ -5,7 +5,7 @@ require 'active_support'
 module ActiveSupportDeprecationReporting
   def warn(message = nil, callstack = nil)
     message = super
-    raise message unless message.blank?
+    raise message unless message.to_s.empty?
   end
 end
 ActiveSupport::Deprecation::Reporting.prepend(ActiveSupportDeprecationReporting)
